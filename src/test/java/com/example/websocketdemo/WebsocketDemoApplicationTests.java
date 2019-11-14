1c618e399d497d25382693d9b3fcbe68d8f92f16
Yogesh Sharma <yogeshniit2016@gmail.com>
2019-11-14 10:15:32 +0530

    Add files via upload

--- src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
+++ src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,17 @@ public class WebsocketDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("Running testcases...");
+	    assertEquals(true, new WebsocketDemoApplicationTests().checkEquality(1,1)); 	
+	    assertEquals(false, new WebsocketDemoApplicationTests().checkEquality(23,3)); 	
+	    assertEquals(false, new WebsocketDemoApplicationTests().checkEquality(12,5)); 	
+	    assertEquals(true, new WebsocketDemoApplicationTests().checkEquality(45,45)); 	
+	    assertEquals(true, new WebsocketDemoApplicationTests().checkEquality(10,10)); 	
+	System.out.println("Testcases Finished...");
+	    
+	}
+	public boolean checkEquality(int a,int b){
+		return a==b;
 	}
 
 }
